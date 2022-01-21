5b5ae28e100d0e152ba79d6faa2077ccc911c1f9
MiquelAngelMontero <mmontero2@institutjaumehuguet.cat>
2022-01-21 11:23:34 +0100

    Milestone 1

--- src/Coche.java
+++ src/Coche.java
@@ -1,4 +1,18 @@
 
-public class Coche {
+public class Coche extends Vehiculo {
+	
+	//En esta clase solo necessitamos un constructor porque todo lo demàs esta implementado en la clase Vehiculo
+	public Coche(String matricula, String marca, Rueda trasera, Rueda delantera, String color) {
+		super(matricula, marca, trasera, delantera, color);
+		// TODO Auto-generated constructor stub
+	}
 
+	@Override
+	public String toString() {
+		return "Coche [matricula=" + matricula + ", marca=" + marca + ", trasera=" + trasera + ", delantera="
+				+ delantera + ", color=" + color + "]";
+	}
+	
+	
+	
 }
--- src/Moto.java
+++ src/Moto.java
@@ -1,4 +1,11 @@
 
-public class Moto {
+public class Moto extends Vehiculo {
 
+	//En esta clase solo necessitamos un constructor porque todo lo demàs esta implementado en la clase Vehiculo
+	public Moto(String matricula, String marca, Rueda trasera, Rueda delantera, String color) {
+		super(matricula, marca, trasera, delantera, color);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
 }
